64c6e7811e40f6e23f268707c523e5eeb7fea30a
enriquesouza <enriquesouza>
2019-12-08 16:59:47 +0100

    qrCodeParser

--- services/smartpay/index.js
+++ services/smartpay/index.js
@@ -16,7 +16,7 @@ async function pay(_password, _addressFrom, _amountTo, _addressTo, _wif) {
     return decryptPK;
 
 }
-
+/*
 let qrCode = "smartcash:SfuQ4fg5ARKaXoKsSD9BnxN4N7DubnqMEp?card=2a8771425c214abc53f82efc54c7c9e0c2f0926105777012193bd73b6e7ede81bcf61372888112e2bdb18c3a5f9b54ebc79209a6a65305255c5c829f9fc4248d"
 let parsedQrCode = parseCardQrCode(qrCode);
 console.log(parsedQrCode);
@@ -24,3 +24,8 @@ console.log(parsedQrCode);
 qrCode = "smartcash:SfuQ4fg5ARKaXoKsSD9BnxN4N7DubnqMEp"
 parsedQrCode = parseCardQrCode(qrCode);
 console.log(parsedQrCode);
+
+qrCode = "smartcash:SfuQ4fg5ARKaXoKsSD9BnxN4N7DubnqMEp?amount=1000&message=oi"
+parsedQrCode = parseCardQrCode(qrCode);
+console.log(parsedQrCode);
+*/
