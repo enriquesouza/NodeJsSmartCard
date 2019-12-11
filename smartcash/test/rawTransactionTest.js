c26012feff74a501d69729f434552644a54cce97
enriquesouza <enriquesouza>
2019-12-11 15:33:22 +0100

    sapi broadcast transaction

--- smartcash/test/rawTransactionTest.js
+++ smartcash/test/rawTransactionTest.js
@@ -4,29 +4,22 @@ const smartCash = require('smartcashjs-lib');
 const rpc = require('../../rpc');
 const request = require('request-promise');
 const _ = require('lodash');
-
-/*
-
-
-rpc.command("sendrawtransaction", signedTx, false).then(rpcResult => {
-
-    console.log(rpcResult)
-
-}).catch(err => console.log(err.message));
-
-//94e92a4db4f1d78228b553da17bd91d3315362d452f64bb9921b40639298d99e
-*/
+const convert = require('../../lib/units')
 
 async function sendrawtransaction() {
 
     let to = "SgPMhNeG16Ty6VaPSnAtxNJAQ2JRnhTGaQ";
-    let amount = 100000;
+    let amount = 0.002;
     let key = process.env.TEST_PK;
 
     createRawTransaction(to, amount, key);
 }
 
 async function createRawTransaction(toAddress, amount, keyString) {
+
+    let satoshi = 100000000;
+
+    let amountSat = satoshi * amount;
 
     let key = smartCash.ECPair.fromWIF(keyString);
 
@@ -40,38 +33,80 @@ async function createRawTransaction(toAddress, amount, keyString) {
 
     console.log(`Total Unspent ${totalUnspent}`)
 
+    let fee = calculateFee(listUnspent);
+
+    console.log(`Fee ${fee}`)
+
     let countUnspent = listUnspent.length;
 
     console.log(`Count Unspent ${countUnspent}`)
     //SEND TO
-    transaction.addOutput(toAddress, amount);
+    transaction.addOutput(toAddress, amountSat);
 
-    for (let i = 0; i < countUnspent; i++) {
+    let change = (totalUnspent - amount - fee);
 
-        let uxto = listUnspent[i];
+    console.log(`change ${change}`)
 
-        console.log(`${uxto.txid} - ${uxto.vout}`)
+    let amountWithFee = amount + fee;
 
-        //TODO: get the list of all UXTO
-        transaction.addInput(uxto.txid, uxto.vout);
+    console.log(`amountWithFee ${amountWithFee}`)
 
+    //Change TO
+    transaction.addOutput(fromAddress, roundUp(change * satoshi, 4));
+
+    let data = new Buffer("smartPay");
+    let ret = smartCash.script.compile(
+        [
+            smartCash.opcodes.OP_RETURN,
+            data
+        ])
+
+    transaction.addOutput(ret, 0)
+
+    let bigInputs = _.find(listUnspent, function (o) {
+        return o.amount >= amountWithFee;
+    });
+
+    console.log(`bigInputs ${JSON.stringify(bigInputs)}`)
+
+    let uxto = null;
+
+    if (!_.isUndefined(bigInputs) && _.isArray(bigInputs)) {
+
+        uxto = _.first(bigInputs);
+
+        console.log(`uxto ${JSON.stringify(uxto)}`);
+    } else {
+        if (!_.isUndefined(bigInputs)) {
+            uxto = bigInputs;
+            console.log(`uxto ${JSON.stringify(uxto)}`);
+        }
     }
 
-    
+    transaction.addInput(uxto.txid, uxto.vout);
+
     try {
         transaction.sign(0, key);
+        
         let signedTransaction = transaction.build().toHex();
+
         console.log(signedTransaction)
+
+        let txid =  await sendTransaction(signedTransaction)
+
+        console.log(txid)
+
+        return txid
+
     } catch (err) {
         console.error(err);
+        throw err;
     }
 }
-/*
-async function calculateFee(listUnspent, amount) {
 
-    let fee = 0.001;
+function calculateFee(listUnspent) {
 
-    let totalUnspent = _.sumBy(listUnspent, 'amount');
+    let fee = 0.002;
 
     let countUnspent = listUnspent.length;
 
@@ -82,16 +117,14 @@ async function calculateFee(listUnspent, amount) {
     if (newFee > fee)
         fee = newFee;
 
-    fee = roundUp(fee, 4);
-
-    let change = (totalUnspent - amount - fee);
+    return roundUp(fee, 4);
 }
 
 function roundUp(num, precision) {
     precision = Math.pow(10, precision)
     return Math.ceil(num * precision) / precision
 }
-*/
+
 
 async function getUnspent(address) {
     let url = `https://insight.smartcash.cc/api/addr/${address}/utxo`;
@@ -99,6 +132,25 @@ async function getUnspent(address) {
         return await request.get(url, {
             json: true
         });
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function sendTransaction(hex) {
+    var options = {
+        method: 'POST',
+        uri: 'http://sapi.dustinface.me/v1/transaction/send',
+        body: {
+            data: `${hex}`,
+            instantpay:false,
+            overrideFees:false
+        },
+        json: true // Automatically stringifies the body to JSON
+    };
+
+    try {
+        return await request.post(options);
     } catch (err) {
         throw err;
     }
